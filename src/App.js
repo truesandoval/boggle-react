8b8aad378c6e51aab0fd51679193410284bca2d6
Krista Katzenmeyer <krissyishere@gmail.com>
2020-01-29 11:21:10 -0800

    Compute all solutions on load
    Adding useEffect which will run only on component initalized. This will populate allSolutions state with all valid solutions for a default board.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import findAllSolutions from './solver.js';
 import './App.css';
 
+const BOARD = [['h', 'i', 's'],['s', 'h', 'e'],['t', 'e', 'a']];
+
 function App() {
+
+  const [allSolutions, setAllSolutions] = useState([]);
+
+  // useEffect will trigger when the array items in the second argument are
+  // updated. The array is empty, so this will run only when component is
+  // initalized. We should only recompute all solutions if the board changes.
+  useEffect(() => {
+    const wordList = require('./full-wordlist.json');
+    let tmpAllSolutions = findAllSolutions(BOARD, wordList.words);
+    setAllSolutions(tmpAllSolutions);
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul>
+        {allSolutions.map((solution) => {return <li>{solution}</li>})}
+      </ul>
     </div>
   );
 }
